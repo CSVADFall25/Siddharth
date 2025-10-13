5ce7845fc0214ef104ca049f6b6f95ca60d58ab8
Siddharth Chattoraj <yeahsidc@gmail.com>
2025-10-13 02:09:44 -0700

    Added color-changing dots

--- mini-assignments/mini-assignment3/sketch.js
+++ mini-assignments/mini-assignment3/sketch.js
@@ -1,5 +1,6 @@
 /* 
-I inverted and mirroed the video.
+I mirrored the video.
+I changed the edges to blue dots and made it so motion means dots outlining edges moving become warmer (based on how fast).
 */
 // IN PROGRESS
 
@@ -11,6 +12,9 @@ var buffer;
 var result;
 var w = 640,
     h = 480;
+
+var grayNow, grayPrev;
+var sampleStep = 3;
 
 function setup() {
     capture = createCapture({
@@ -27,6 +31,11 @@ function setup() {
     capture.size(w, h);
     capture.hide();
     buffer = new jsfeat.matrix_t(w, h, jsfeat.U8C1_t);
+    grayNow  = new jsfeat.matrix_t(w, h, jsfeat.U8C1_t);
+    grayPrev = new jsfeat.matrix_t(w, h, jsfeat.U8C1_t);
+
+    colorMode(HSB, 255);
+    noStroke();
 }
 
 function jsfeatToP5(src, dst) {
@@ -49,25 +58,27 @@ function jsfeatToP5(src, dst) {
 }
 
 function draw() {
-    image(capture, 0, 0, 640, 480);
+    background(0); // only dots visible
     capture.loadPixels();
     if (capture.pixels.length > 0) { // don't forget this!
-        var blurSize = select('#blurSize').elt.value;
-        var lowThreshold = select('#lowThreshold').elt.value;
-        var highThreshold = select('#highThreshold').elt.value;
+        var blurSize = select('#blurSize') ? select('#blurSize').elt.value : 50;
+        var lowThreshold = select('#lowThreshold') ? select('#lowThreshold').elt.value : 20;
+        var highThreshold = select('#highThreshold') ? select('#highThreshold').elt.value : 60;
 
         blurSize = map(blurSize, 0, 100, 1, 12);
         lowThreshold = map(lowThreshold, 0, 100, 0, 255);
         highThreshold = map(highThreshold, 0, 100, 0, 255);
 
-        jsfeat.imgproc.grayscale(capture.pixels, w, h, buffer);
+        jsfeat.imgproc.grayscale(capture.pixels, w, h, grayNow);
+
+        buffer.data.set(grayNow.data);
         jsfeat.imgproc.gaussian_blur(buffer, buffer, blurSize, 0);
         jsfeat.imgproc.canny(buffer, buffer, lowThreshold, highThreshold);
         var n = buffer.rows * buffer.cols;
         // Invert image
-        for (var i = 0; i < n; i++) {
-            buffer.data[i] = 255 - buffer.data[i];
-        }
+        // for (var i = 0; i < n; i++) {
+        //     buffer.data[i] = 255 - buffer.data[i];
+        // }
         // Result
         result = jsfeatToP5(buffer, result);
 
@@ -75,7 +86,24 @@ function draw() {
         push();
         translate(width, 0);
         scale(-1, 1);
-        image(result, 0, 0, 640, 480);
+
+        // Dots!
+        var maxMotion = 60;
+        for (var y = 0; y < h; y += sampleStep) {
+            var rowIndex = y * w;
+            for (var x = 0; x < w; x += sampleStep) {
+                var idx = rowIndex + x;
+                if (buffer.data[idx] !== 0) {
+                    var m = Math.abs((grayNow.data[idx] | 0) - (grayPrev.data[idx] | 0));
+                    if (m > maxMotion) m = maxMotion;
+                    var hueVal = map(m, 0, maxMotion, 170, 0);
+                    fill(hueVal, 255, 255);
+                    ellipse(x, y, 8, 8);
+                }
+            }
+        }
         pop();
+
+        grayPrev.data.set(grayNow.data);
     }
 }
